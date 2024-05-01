cc1a7dd6b551fba41b7bbd5149b79e20ecd379ef
ezyxip <anzej2003@gmail.com>
2024-05-01 20:14:36 +0600

    Some changes

---
+++ app/src/main/java/com/ezyxip/veko/screens/MainScreen.kt
@@ -0,0 +1,9 @@
+package com.ezyxip.veko.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier){
+
+}
