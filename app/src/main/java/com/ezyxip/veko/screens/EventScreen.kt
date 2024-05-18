36ff86eb614258b4f4454038ff691a637755eddf
ezyxip <anzej2003@gmail.com>
2024-05-18 20:02:24 +0600

    Начало вёрстки

--- app/src/main/java/com/ezyxip/veko/screens/EventScreen.kt
+++ app/src/main/java/com/ezyxip/veko/screens/EventScreen.kt
@@ -1,11 +1,25 @@
 package com.ezyxip.veko.screens
 
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import com.ezyxip.veko.components.Menuable
 
 @Composable
 fun EventScreen(
-    navigate: (String) -> Unit,
+    navigator: (String) -> Unit,
 ){
-    Text("Event screen")
+    Menuable (
+        title = "События",
+        navigator = navigator,
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
+    ) {
+
+    }
 }
